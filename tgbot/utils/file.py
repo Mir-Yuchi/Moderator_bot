895548313f32c882fe864d3956e97f2d1c6d6101
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-10-25 20:52:02 +0500

    changed mat comparision

--- tgbot/utils/file.py
+++ tgbot/utils/file.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from Levenshtein import ratio
 
 
-def levenstein_range(phrase: str, word: str, percent: float = .0):
+def levenstein_range(phrase: str, word: str, percent: float = .90):
     for part in range(len(phrase)):
         fragment = phrase[part: part + len(word)]
         distance = ratio(fragment, word)
@@ -12,18 +12,18 @@ def levenstein_range(phrase: str, word: str, percent: float = .0):
 
 
 def detect_obvious_word(obscene_words_file_path: str | Path,
-                        phrase: str, percent: float = .0) -> bool:
+                        phrase: str, percent: float = .90) -> bool:
     with open(obscene_words_file_path, encoding='utf-8') as file:
         for word in file:
-            if len(word) <= 0:
-                percent = .0
+            if len(word) <= 5:
+                percent = .90
             if levenstein_range(phrase, word, percent):
                 return True
     return False
 
 
 def detect_obv_list_word(word_list: list[str], text: str,
-                         percent: float = .0) -> bool:
+                         percent: float = .9) -> bool:
     for word in word_list:
         for part in range(len(text)):
             fragment = text[part: part + len(word)]
